557c7e28468c56b07594ccb0411353da3c280c27
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2019-07-11 23:03:51 +0700

    Setup pubspec.yaml file for dependency android wear
    Setup pubspec.yaml file for dependency android wear and setup MainActivity.kt file

--- android/app/src/main/kotlin/com/ysn/flutter_wear_countdown/MainActivity.kt
+++ android/app/src/main/kotlin/com/ysn/flutter_wear_countdown/MainActivity.kt
@@ -1,13 +1,23 @@
 package com.ysn.flutter_wear_countdown
 
 import android.os.Bundle
+import android.support.wear.ambient.AmbientMode
+import com.mjohnsullivan.flutterwear.wear.FlutterAmbientCallback
+import com.mjohnsullivan.flutterwear.wear.getChannel
 
 import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterActivity(), AmbientMode.AmbientCallbackProvider {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     GeneratedPluginRegistrant.registerWith(this)
+
+    // Wire up the activity for ambient callbacks
+    AmbientMode.attachAmbientSupport(this)
+  }
+
+  override fun getAmbientCallback(): AmbientMode.AmbientCallback {
+      return FlutterAmbientCallback(getChannel(flutterView))
   }
 }
